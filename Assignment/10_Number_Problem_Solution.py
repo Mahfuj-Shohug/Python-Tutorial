f9dcd0f155cb79cfe36ed7fccea27e35b6f2b952
Mahfuj-Shohug <mahfuj.shohug@gmail.com>
2021-09-23 23:26:30 +0600

    10th number problem solution of the given assignment

---
+++ Assignment/10_Number_Problem_Solution.py
@@ -0,0 +1,8 @@
+'''
+Problem number 10:
+Access value 20 from the following 
+tuple -> aTuple = ("Orange", [10, 20, 30], (5, 15, 25))
+'''
+a_tuple = ("Orange", [10, 20, 30], (5, 15, 25))
+# Access value 20
+print(a_tuple[1][1])
