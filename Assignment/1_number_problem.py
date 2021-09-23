4216f23d565e5eab52b9e824cfb93eb85b58072f
Mahfuj-Shohug <mahfuj.shohug@gmail.com>
2021-09-23 19:04:31 +0600

    1st problem solution of the given assignment

---
+++ Assignment/1_number_problem.py
@@ -0,0 +1,12 @@
+'''
+Problem: Given two integer numbers return their product. 
+If the product is greater than 1000, then return their sum.
+'''
+
+num_1 = 1000
+num_2 = 500
+product = 1100
+if (product > 1000):
+    print('Sum of those integer = ', num_1+num_2)
+else:
+    print('Their product is = ', product)
