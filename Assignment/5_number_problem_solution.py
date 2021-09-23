7f528cb74b2a3b97204a4604262874a201abfc48
Mahfuj-Shohug <mahfuj.shohug@gmail.com>
2021-09-23 19:48:03 +0600

    5th problem solution of the given assignment

---
+++ Assignment/5_number_problem_solution.py
@@ -0,0 +1,13 @@
+'''
+Problem 5:
+Given a list, iterate it, and display numbers divisible by five, 
+and if you find a number greater than 150, stop the loop iteration.
+'''
+list_num = [20, 33, 40, 54, 60, 70, 82, 88, 90, 122, 130, 142, 155]
+for i in list_num:
+    if i < 150:
+        if (i % 5 == 0):
+            print('Number is divided by 5 = ', i)
+    else:
+        print('Oh no this is gatter then 150')
+        break
