6cb36c2e637647f85e1c555448da953254cdff00
Mahfuj-Shohug <mahfuj.shohug@gmail.com>
2021-11-06 00:30:07 +0600

    Global Variable and Local Variable

--- 5th Class/oop.py
+++ 5th Class/oop.py
@@ -2,3 +2,30 @@
 # 1. Hip Memory : Useable memory 
 # 2. Stack memory: Constant type or static type or non changeable
 # To use OOP programming using Hip memory
+# Lesson: OOP-> Class-> OOP Principal 
+# class is an area and a blue ptint of OOP
+
+class Student:
+    # Class properties -> attributes, Function
+    # Attributes -> Variable -> 1. Local Variable 2. Global Variable
+    #Global Variable 
+    name = ''
+    dept = ''
+
+    # Constructor 
+    # Class must need a construtor 
+    def __init__(self):
+        pass
+    #function
+    def getName(self):
+        return self.name
+
+    def setName(self, name):
+        self.name = name
+
+    def universityName(self):
+        #local Variable 
+        varsityName = 'DIU'
+
+    
+
