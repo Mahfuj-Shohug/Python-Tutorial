15352971cd611ca40cc2f6fc4e389815a541768c
Mahfuj-Shohug <mahfuj.shohug@gmail.com>
2021-11-06 00:20:12 +0600

    OOP concept

---
+++ 5th Class/oop.py
@@ -0,0 +1,4 @@
+# Prgramming memory two types:
+# 1. Hip Memory : Useable memory 
+# 2. Stack memory: Constant type or static type or non changeable
+# To use OOP programming using Hip memory
---
+++ 8th Class/mysql_class.py
@@ -0,0 +1,7 @@
+# Data base
+# two types of data base
+# 1. SQL(MySql), 2. NoSql(Cullection, Dectionalry, MongoDB)
+# Handeling Database with Python
+# ORM system
+#install msql 
+
